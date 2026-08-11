1116bd322d7cb65e6d8a5e0fc6c15dcb6ae8d08a
xranZhao <xranzhao@users.noreply.github.com>
2026-08-11 14:34:37 +0800

    优化手机端界面并补齐PWA安装

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'weijin-v1';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './fonts/weijin-brand.woff2'];
+const CACHE = 'weijin-v2';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './fonts/weijin-brand.woff2', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
